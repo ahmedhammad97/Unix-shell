f8ae70e8da6e02cdab43092c7515550372d07e7b
Ahmed Hammad <hammad97official@gmail.com>
2018-10-24 12:24:04 +0200

    Skip empty lines

--- app/main.cpp
+++ app/main.cpp
@@ -5,23 +5,26 @@ using namespace std;
 
 void welcomeScreen(){
     cout<<"*********************************\n";
-    cout<<"Welcome to this simple Unix Shell\n";
-    cout<<"\nMade by: Ahmed Hammad - 4268\n";
+    cout<<"Welcome to this Simple Unix Shell\n";
+    cout<<"\nCoded by: Ahmed Hammad - 4268\n";
     cout<<"*********************************\n";
 }
 
 string scanCommands(){
-    string input;
-    input.reserve(50);
-    getline(cin, input);
+    string input=" ";
+    while(getline(cin, input)){
+        if(input.empty()) continue;
+        else break;
+    }
     return input;
 }
 
-vector<string> parseInput(string input){ //To be edited (Multiple spaces) ()
+vector<string> parseInput(string input){
     vector<string> result;
     string temp = "";
     for(int i=0; i<input.size(); i++){
         if(input[i]==' '){
+			if(i==0 || input[i-1]==' '){continue;}
             result.push_back(temp);
             temp="";
         }
@@ -34,7 +37,7 @@ vector<string> parseInput(string input){ //To be edited (Multiple spaces) ()
 }
 
 bool validate(vector<string> commands){
-    if(commands[0][0] == '-']){return false;}
+    if(commands[0][0] == '-'){return false;}
     for(int i=1; i<commands.size(); i++){
         if(commands[i]=="&"){continue;}
         if(commands[i].size()==2 && commands[i][0]=='-'){continue;}
@@ -49,6 +52,7 @@ int main(){
 	vector<string> commands;
 	do{
         commands = parseInput(scanCommands());
+        if(commands.empty()){continue;}
 
         if(validate(commands)){
             cout<<commands[0]<<endl;
